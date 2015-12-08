102d3097f1f0764b578a44a099a2777c112e7c0a
stphnzilch <stephenz@hawaii.edu>
2015-12-07 20:57:58 -1000

    added melting and gs content functiion

--- primer.py
+++ primer.py
@@ -19,14 +19,20 @@ def linebreak():
     print "\n"
     print "----------------------------"
     print "\n"
+def GCcontent(G,C,frwd):
+      G_C = 100 * float(G + C) / len(frwd)  
+      print "GC content", G_C, "%"
 
+def melttemp(G,C,A,T):
+      MT = 64.9 + 41 *float( (G + C - 16.4) / (A + T + G + C))
+      print  "melting temp", MT    
+def main():
+    seq = ''
 
-seq = ''
-
-parser = argparse.ArgumentParser()
-parser.add_argument("bases")
-args = parser.parse_args()
-with open(args.bases, 'r') as fasta:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bases")
+    args = parser.parse_args()
+    with open(args.bases, 'r') as fasta:
         for line in fasta:
             if line.startswith(">"):
                
@@ -34,64 +40,60 @@ with open(args.bases, 'r') as fasta:
             else:
                 seq += line.strip()
 
+                
+    just_restriction = Restriction.HindIII
+    restriction_enzyme = just_restriction
 
-just_restriction = Restriction.HindIII
-
-restriction_enzyme = just_restriction
-
-res_site =  Seq(restriction_enzyme.site)
-
-
-forward_resriction = res_site
-backward_restriction = res_site.reverse_complement()
-print just_restriction
-
-sequence_sites = forward_resriction + seq + backward_restriction
-
-print "restriction site", restriction_enzyme.site   #just the name of the input sequance
-
-linebreak()
-
-print name
-print "Length", len(sequence_sites)                 # just the input sequancse                 
-linebreak()
-
-frwd = sequence_sites[0:20]   
-
-print  name, ", forward primer"  #frwd_raw_res       print ">forward", name , #frwd_raw_
+    res_site =  Seq(restriction_enzyme.site)
 
 
-print frwd
+    forward_resriction = res_site
+    backward_restriction = res_site.reverse_complement()
+    print just_restriction
 
-frwd_G = frwd.count("G")
-frwd_C = frwd.count("C")
-frwd_A = frwd.count("A")
-frwd_T = frwd.count("T")
+    sequence_sites = forward_resriction + seq + backward_restriction
+
+    print "restriction site", restriction_enzyme.site   #just the name of the input sequance
+
+    linebreak()
+
+    print name
+    print "Length", len(sequence_sites)                 #  this section  shows the length and name of the input sequance
+    linebreak()
+
+    frwd = sequence_sites[0:20]   
+
+    print  name, ", forward primer"  #prints the forward primer and the meling point
 
 
-frwdG_C = 100 * float(frwd_G + frwd_C) / len(frwd)  
+    print "forward", frwd
 
-print "GC content", frwdG_C, "%"
+    G = frwd.count("G")
+    C = frwd.count("C")
+    A = frwd.count("A")
+    T = frwd.count("T")
 
-frwd_MT = 64.9 + 41 *float( (frwd_G + frwd_C - 16.4) / (frwd_A + frwd_T + frwd_G + frwd_C))
+    GCcontent(G,C,frwd)
 
-print  "melting temp", frwd_MT  
-
-linebreak()
+    melttemp(G,C,A,T)
 
 
-revs = sequence_sites[-20:]                                       #sequace for the reverse primer
-revs_revs_comp = revs.reverse_complement()                            #reverse complement of the last 20 characters
+
+    linebreak()       #prints the sequnce an meling point and gc content
+
+
+    revs = sequence_sites[-20:]                                       #sequace for the reverse primer
+    revs_revs_comp = revs.reverse_complement()                            #reverse complement of the last 20 characters
                                                                        #documentation same as forward
 
-print   name, ", reverse primer"
-print revs_revs_comp
+    print   name, ", reverse primer"
+    print revs_revs_comp
 
-revs_G = revs_revs_comp.count("G") 
-revs_C = revs_revs_comp.count("C")
-revs_A = revs_revs_comp.count("A")
-revs_T = revs_revs_comp.count("T")
-revsG_C = 100 * float (revs_G + revs_C ) / len(revs_revs_comp)
-print "GC content: ",revsG_C, "%"
-revs_MT =  64.9 + 41 *float( (revs_G + revs_C - 16.4) / (revs_A + revs_T + revs_G + revs_C))    #http://www.biophp.org/minitools/melting_temperature/demo.php?formula=basic
-print "melting temp",  revs_MT
+    G = revs_revs_comp.count("G") 
+    C = revs_revs_comp.count("C")
+    A = revs_revs_comp.count("A")
+    T = revs_revs_comp.count("T")
+    GCcontent(G,C,frwd)
+    melttemp(G,C,A,T)
+
+main()        #EVERTHING!!!!!!
