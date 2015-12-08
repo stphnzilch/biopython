4f8f5b5ef52e7f35c75d6778af075c9a14837afb
stphnzilch <stephenz@hawaii.edu>
2015-12-08 11:07:20 -1000

    now with the restriction toss function

--- primer.py
+++ primer.py
@@ -4,29 +4,22 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC  #from biopython tutorial 3.3
 from Bio import Restriction
 from Bio.Restriction import Restriction_Dictionary
-
-
-#name= raw_input("name sequance:  ") 
-#raw_sequance = raw_input("enter sequance: ")
-#raw_res = raw_input("enter restriction site(HindIII, EcoRI): ") 
-
-import sys
-import random
 import argparse
 
-
+#linebreak prints a an empty line and a dotted line
 def linebreak():
     print "\n"
     print "----------------------------"
     print "\n"
+#GCcontent is a simple fraction that divides the amont of gs and cs and divides them by the whole length of the primer
 def GCcontent(G,C,frwd):
       G_C = 100 * float(G + C) / len(frwd)  
       print "GC content", G_C, "%"
-
+#meltingtemp uses a function that does syuff takes in a few variables and prints the thing
 def melttemp(G,C,A,T):
       MT = 64.9 + 41 *float( (G + C - 16.4) / (A + T + G + C))
       print  "melting temp", MT    
-
+#countbase sets the global variable of GCTA and count them in the seq
 def countbase(seq):
       global G
       G = seq.count("G")
@@ -36,9 +29,21 @@ def countbase(seq):
       A = seq.count("A")
       global T
       T = seq.count("T")
+#
+def restriction_toss():    
+    global forward_resriction
+    global backward_restriction
+    just_restriction = Restriction.HindIII
+    restriction_enzyme = just_restriction
+    res_site =  Seq(restriction_enzyme.site)
+    forward_resriction = res_site
+    backward_restriction = res_site.reverse_complement()
+    print just_restriction
+    print "restriction site", restriction_enzyme.site   #just the name of the input sequance
+    
 
 def main():
-    seq = ''
+    sequ = ''
 
     parser = argparse.ArgumentParser()
     parser.add_argument("bases")
@@ -49,17 +54,11 @@ def main():
                
                 name = (line.strip())
             else:
-                seq += line.strip()
+                sequ += line.strip()
 
-                
-    just_restriction = Restriction.HindIII
-    restriction_enzyme = just_restriction
-    res_site =  Seq(restriction_enzyme.site)
-    forward_resriction = res_site
-    backward_restriction = res_site.reverse_complement()
-    print just_restriction
-    sequence_sites = forward_resriction + seq + backward_restriction
-    print "restriction site", restriction_enzyme.site   #just the name of the input sequance
+    restriction_toss()
+    
+    sequence_sites = forward_resriction + sequ + backward_restriction
 
     linebreak()
 
