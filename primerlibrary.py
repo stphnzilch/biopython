58782d2e14f625c119f0a85db2c1e54f57aaa1f7
stphnzilch <stephenz@hawaii.edu>
2015-12-08 14:12:51 -1000

    first commit in a primer library

---
+++ primerlibrary.py
@@ -0,0 +1,79 @@
+#linebreak prints a an empty line and a dotted line
+def linebreak():
+    print "\n"
+    print "----------------------------"
+    print "\n"
+
+
+#GCcontent is a simple fraction that divides the amont of gs and cs and divides them by the whole length of the primer
+def GCcontent(G,C,frwd):
+      G_C = 100 * float(G + C) / len(frwd)  
+      print "GC content", G_C, "%"
+
+
+#meltingtemp uses a function that does syuff takes in a few variables and prints the thing
+def melttemp(G,C,A,T):
+      MT = 64.9 + 41 *float( (G + C - 16.4) / (A + T + G + C))
+      print  "melting temp", MT
+
+    
+#countbase sets the global variable of GCTA and count them in the seq
+def countbase(seq):
+      global G
+      global C
+      global A
+      global T
+      G = seq.count("G")
+      C = seq.count("C")
+      A = seq.count("A")
+      T = seq.count("T")
+
+#restriction toss requires no input variables if the 
+#restriction site needs to be changed it must be done in this function.
+#first it defines the global variables
+#then it names a variable the restriction enzyme pulling in 
+#formation from a biopython dictionary
+#then the name is turned into a the sequance of bases and converted to a string
+#then the name and the seqaunce is printed.
+#the second part does the same thing but resse complements the sequance
+def restriction_toss():    
+    global forward_res_site
+    global backward_restriction
+    global reverse_restriction_enzyme
+    global forward_restriction_enzyme
+
+    forward_res_name = Restriction.HindIII
+    forward_res_site =  Seq(forward_res_name.site)
+    print forward_res_name
+    print "restriction site", forward_res_name.site   
+   
+    reverse_res_name = Restriction.EcoRI
+    reverse_res_site =  Seq( reverse_res_name.site)
+    backward_restriction = reverse_res_site.reverse_complement()
+    print reverse_res_name
+    print "restriction site",  reverse_res_name.site   
+#divides the data coming in
+#if the line stars with a > then its a titile
+#then everything else is a base sequance
+#store variable name and sequ
+def parsing():
+    global sequ
+    global name
+    global parser
+    sequ = ''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bases")
+    args = parser.parse_args()
+    with open(args.bases, 'r') as fasta:
+        for line in fasta:
+            if line.startswith(">"):
+               
+                name = (line.strip())
+            else:
+                sequ += line.strip()
+
+#adds the restriction sites to the original sequance
+def seq_adding():
+    global sequence_sites
+    sequence_sites = forward_res_site + sequ + backward_restriction
+
